4232b5fff80df25ba4acbe435f7a3f3f60ecbc6d
oooo <bjoerngottschall@gmail.com>
2017-02-06 16:06:30 +0100

    form1

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,7 @@
 <?php
 // Email Submit
 // Note: filter_var() requires PHP >= 5.2.0
-if ( isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['project_name']) && isset($_POST['description'])  && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+if ( isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['project_name']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
     // detect & prevent header injections
     $test = "/(content-type|bcc:|cc:|to:)/i";
     foreach ( $_POST as $key => $val ) {
@@ -23,7 +23,7 @@ Project Name: $project_name\n
 Description: $description
 ";
 
-mail( "spam@choyan.me", $_POST['date'], $message, "From:" . $_POST['email'] );
+mail( "bjoerngottschall@gmail.com", $_POST['date'], $message, "From:" . $_POST['email'] );
   //            ^
   //  Replace with your email
 }
